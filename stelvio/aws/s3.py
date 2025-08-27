2a664c42636da6076bed0b73880826aa6d5929fa
Bas Steins <592313+sebst@users.noreply.github.com>
2025-08-27 15:42:29 +0200

    PR comments

--- stelvio/aws/s3.py
+++ stelvio/aws/s3.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-from typing import final
+from typing import Literal, final
 
 import pulumi
 import pulumi_aws
@@ -13,19 +13,18 @@ from stelvio.link import Link, Linkable, LinkConfig
 @dataclass(frozen=True)
 class S3BucketResources:
     bucket: pulumi_aws.s3.Bucket
+    public_access_block: pulumi_aws.s3.BucketPublicAccessBlock
+    bucket_policy: pulumi_aws.s3.BucketPolicy | None
 
 
 @final
 class Bucket(Component[S3BucketResources], Linkable):
     def __init__(
-        self,
-        name: str,
-        versioning_enabled: bool = False,
-        prevent_public_access: bool = True,
+        self, name: str, versioning_enabled: bool = False, access: Literal["public"] | None = None
     ):
         super().__init__(name)
         self.versioning_enabled = versioning_enabled
-        self.prevent_public_access = prevent_public_access
+        self.access = access
         self._resources = None
 
     def _create_resources(self) -> S3BucketResources:
@@ -36,20 +35,53 @@ class Bucket(Component[S3BucketResources], Linkable):
         )
 
         # Configure public access block
-        public_access_block = pulumi_aws.s3.BucketPublicAccessBlock(
-            context().prefix(f"{self.name}-pab"),
-            bucket=bucket.id,
-            block_public_acls=self.prevent_public_access,
-            block_public_policy=self.prevent_public_access,
-            ignore_public_acls=self.prevent_public_access,
-            restrict_public_buckets=self.prevent_public_access,
-        )
+        if self.access == "public":
+            # setup readonly configuration
+            public_access_block = pulumi_aws.s3.BucketPublicAccessBlock(
+                context().prefix(f"{self.name}-pab"),
+                bucket=bucket.id,
+                block_public_acls=False,
+                block_public_policy=False,
+                ignore_public_acls=False,
+                restrict_public_buckets=False,
+            )
+            public_read_policy = pulumi_aws.iam.get_policy_document(
+                statements=[
+                    {
+                        "effect": "Allow",
+                        "principals": [
+                            {
+                                "type": "*",
+                                "identifiers": ["*"],
+                            }
+                        ],
+                        "actions": ["s3:GetObject"],
+                        "resources": [self.bucket.arn.apply(lambda arn: f"{arn}/*")],
+                    }
+                ]
+            )
+            bucket_policy = pulumi_aws.s3.BucketPolicy(
+                context().prefix(f"{self.name}-policy"),
+                bucket=self.bucket.id,
+                policy=public_read_policy.json,
+            )
+            pulumi.export(f"s3bucket_{self.name}_policy_id", bucket_policy.id)
+        else:
+            public_access_block = pulumi_aws.s3.BucketPublicAccessBlock(
+                context().prefix(f"{self.name}-pab"),
+                bucket=bucket.id,
+                block_public_acls=True,
+                block_public_policy=True,
+                ignore_public_acls=True,
+                restrict_public_buckets=True,
+            )
+            bucket_policy = None
 
         pulumi.export(f"s3bucket_{self.name}_arn", bucket.arn)
         pulumi.export(f"s3bucket_{self.name}_name", bucket.bucket)
         pulumi.export(f"s3bucket_{self.name}_public_access_block_id", public_access_block.id)
 
-        return S3BucketResources(bucket)
+        return S3BucketResources(bucket, public_access_block, bucket_policy)
 
     @property
     def arn(self) -> pulumi.Output[str]:
