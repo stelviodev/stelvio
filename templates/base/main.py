7c55dc7958b6b7f4737f0adb5159950e1c659649
Bas Steins <592313+sebst@users.noreply.github.com>
2025-10-24 19:18:52 +0000

    #56: Templates for CLI

---
+++ templates/base/main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from stelvio-template!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ templates/base/stlv_app.py
@@ -0,0 +1,18 @@
+from stelvio.app import StelvioApp
+from stelvio.config import StelvioAppConfig, AwsConfig
+
+app = StelvioApp("stelvio-template")
+
+@app.config
+def configuration(env: str) -> StelvioAppConfig:
+    return StelvioAppConfig(
+        aws=AwsConfig(
+            # region="us-east-1",        # Uncomment to override AWS CLI/env var region
+            # profile="your-profile",    # Uncomment to use specific AWS profile
+        ),
+    )
+
+@app.run
+def run() -> None:
+    # Create your infra here
+    pass
