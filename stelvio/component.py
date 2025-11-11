e29aa00d185b066c7ee3e5c719bc18ef7c7159a0
Bas Steins <592313+sebst@users.noreply.github.com>
2025-11-11 19:56:24 +0000

    Reapply "wip iot things?"
    This reverts commit 9745779c6700753788c86d266e4516d274cfa530.

--- stelvio/component.py
+++ stelvio/component.py
@@ -41,13 +41,19 @@ class TunnelableComponent[ResourcesT](Component[ResourcesT], ABC):
     async def handle_tunnel_event(
         self, data: dict, client: WebsocketClient, logger: TunnelLogger
     ) -> None:
-        # """Handle incoming tunnel event"""
-        # if not self._dev_endpoint_id:
-        #     return
-        # # if data.get("payload", {}).get("endpoint") != self._dev_endpoint_id:
+        """Handle incoming tunnel event"""
+        import time 
+        time.sleep(1)
+        if not self._dev_endpoint_id:
+            print("Return 1")
+            return
+        # if data.get("payload", {}).get("endpoint") != self._dev_endpoint_id:
+        # TODO: Re-enable endpoint check
         # if data.get("endpoint") != self._dev_endpoint_id:
+        #     print("Return 2")
         #     return
-        if data.get("type") == "request-processed":
+        if data.get("type") != "request-received":
+            print("Return 3")
             return
         await self._handle_tunnel_event(data, client, logger)
 
