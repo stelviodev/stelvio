3683f080f1bb5aad6b006dfa2d5b60676f6550c3
Bas Steins <592313+sebst@users.noreply.github.com>
2025-12-04 13:13:37 +0000

    pr comments pt 6

--- stelvio/aws/cloudfront/origins/decorators.py
+++ stelvio/aws/cloudfront/origins/decorators.py
@@ -1,10 +1,9 @@
 from __future__ import annotations
 
+from stelvio.aws.cloudfront.origins.registry import CloudfrontAdapterRegistry
+
 
 def _register_adapter(adapter_cls: type, component_cls: type) -> None:
-    # Import here to avoid circular import
-    from stelvio.aws.cloudfront.origins.registry import CloudfrontAdapterRegistry
-
     adapter_cls.component_class = component_cls
     CloudfrontAdapterRegistry.classes.append(adapter_cls)
 
