d1e2bf47bce3d6c6e9347ba98974b041e0cd6417
Bas Steins <592313+sebst@users.noreply.github.com>
2025-12-30 21:24:01 +0000

    subscribe()

--- stelvio/aws/queue.py
+++ stelvio/aws/queue.py
@@ -5,6 +5,7 @@ import pulumi
 import pulumi_aws
 
 from stelvio import context
+from stelvio.aws.function.function import Function
 from stelvio.aws.permission import AwsPermission
 from stelvio.component import Component, ComponentRegistry, link_config_creator
 from stelvio.dns import Dns, DnsProviderNotConfiguredError
@@ -15,6 +16,7 @@ from stelvio.link import Link, Linkable, LinkConfig
 @dataclass(frozen=True)
 class QueueResources:
     queue: pulumi_aws.sqs.Queue
+    subscriptions: list[pulumi_aws.sqs.QueueEventSubscription] = None
 
 
 @final
@@ -27,7 +29,10 @@ class Queue(Component[QueueResources], Linkable):
         fifo: bool = False,
     ):
         super().__init__(name)
-        
+        self.subscriptions: list[pulumi_aws.sqs.QueueEventSubscription] = []
+        self.delay_seconds = delay_seconds
+        self.visibility_timeout_seconds = visibility_timeout_seconds
+        self.fifo = fifo
         self._resources = None
 
     def _create_resources(self) -> QueueResources:
@@ -39,7 +44,27 @@ class Queue(Component[QueueResources], Linkable):
         )
         return QueueResources(
             queue=queue,
+            subscriptions=self.subscriptions,
         )
+
+
+    def subscribe(self, function: Function | str) -> pulumi_aws.sqs.QueueEventSubscription:
+        i = len(self.subscriptions)
+        if isinstance(function, str):
+            function = Function(
+                name=f"{self.name}-function-{i}",
+                handler=function,
+                links=[self]
+            )
+        subscription = pulumi_aws.sqs.QueueEventSubscription(
+            resource_name=f"{self.name}-subscription-{function.name}",
+            queue=self.resources.queue.id,
+            lambda_function=function.resources.function.arn,
+        )
+        self.subscriptions.append(subscription)
+        return subscription
+
+
 
     def link(self) -> Link:
         link_creator_ = ComponentRegistry.get_link_config_creator(type(self))
